5f33d20329d5a3ce1637095f894747592c4ab2d0
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-07-01 11:46:11 +0530

    Added login url...

--- index.js
+++ index.js
@@ -59,10 +59,16 @@
 
 const http = require('http');
 
-const server = http.createServer((req, res) => {   
-    res.write('<html> <head> <title>My First Web Server</title> </head> <body> <h1>Hello, world!</h1> </body> </html>');
+const server = http.createServer((req, res) => {
+    res.setHeader('Content-Type', 'text/html');
+  
+    if (req.url === '/login') {
+      res.write('<html> <head> <title>Login Page</title> </head> <body> <h1>Login Page</h1> </body> </html>');
+    } else {
+      res.write('<html> <head> <title>My First Web Server</title> </head> <body> <h1>Hello, world!</h1> </body> </html>');
+    }
     res.end();
-});
+  });
 
 const port = 3000;
 const host = 'localhost';
