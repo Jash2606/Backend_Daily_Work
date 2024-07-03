019fd40c806571cf1af7c382b650e085b1050a4a
Jash2606 <jash.23bcs10163@sst.scaler.com>
2024-07-03 11:20:07 +0530

    Created logger middleware..

--- Express-class/index.js
+++ Express-class/index.js
@@ -4,25 +4,41 @@ const fs = require('fs');
 const { Server } = require('https');
 app.use(express.json());
 
+
+// Middleware function
+function middleware(req, res, next) {
+    console.log('Middleware function called...');
+    next();
+}
+app.use(middleware);
+
+
+// Logger middleware
+const logger = (req, res, next) => {
+    const method = req.method;
+    const ip = req.ip;
+    const hostname = req.hostname;
+    const date = new Date().toISOString();
+    
+    console.log(`[${date}] ${method} request from ${ip} to ${hostname}`);
+    next();
+};
+app.use(logger);
+
+
 let courses = [
     {id: 1, name: 'DSA', duration: '3 months'},
-    {id: 2, name: 'WEB_DEV', duration: '2 months'}
-    // {id: 3, name: 'REACT', duration: '1.5 months'},
-    // {id: 4, name: 'NODE_JS', duration: '2 months'},
-    // {id: 5, name: 'EXPRESS', duration: '1 month'},
-    // {id: 6, name: 'MONGODB', duration: '1.5 months'},
-    // {id: 7, name: 'MYSQL', duration: '1 month'},
-    // {id: 8, name: 'PYTHON', duration: '3 months'},
-    // {id: 9, name: 'JAVA', duration: '3 months'},
-    // {id: 10, name: 'C++', duration: '2 months'},
-    // {id: 11, name: 'C', duration: '1 month'},
-    // {id: 12, name: 'RUBY', duration: '1.5 months'}
+    {id: 2, name: 'WEB_DEV', duration: '2 months'},
+    {id: 3, name: 'REACT', duration: '1.5 months'},
+    {id: 4, name: 'NODE_JS', duration: '2 months'},
+    {id: 5, name: 'EXPRESS', duration: '1 month'},
+    {id: 6, name: 'MONGODB', duration: '1.5 months'}
 ];
 
-console.log("Hello World");
+
 app.get('/courses', (req, res) => {
-    console.log(req.body);
-    console.log("Hellow World");
+    // console.log(req.body);
+    // console.log("Hellow World");
     res.json(courses);
     // res.send('Hello World');
 });
@@ -39,14 +55,18 @@ app.post('/courses', (req, res) => {
 
 
 app.put('/courses/:id', (req, res) => {
-    const courseId = parseInt(req.params.id);
-    const course = courses.find(c => c.id === courseId);
-    if (!course) {
-        return res.status(404).send('Course not found');
+    try {
+        const courseId = parseInt(req.params.id);
+        const course = courses.find(c => c.id === courseId);
+        if (!course) {
+            return res.status(404).send('Course not found');
+        }
+        course.name = req.body.name || course.name;
+        course.duration = req.body.duration || course.duration;
+        res.json(course);
+    } catch (error) {
+        res.status(500).send('Internal Server Error');
     }
-    course.name = req.body.name || course.name;
-    course.duration = req.body.duration || course.duration;
-    res.json(course);
 });
 
 
@@ -59,6 +79,8 @@ app.delete('/courses/:id', (req, res) => {
     const deletedCourse = courses.splice(courseIndex, 1);
     res.json(deletedCourse);
 });
+
+
 
 
 const port = 3000;
